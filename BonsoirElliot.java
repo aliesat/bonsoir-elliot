6b3f9cfd2840aeff15413fc6d164ae0327d3a3f2
aliesat <ali.esat.ozbay@student.uva.nl>
2016-11-22 13:32:11 +0100

    main class declared static
    Had not been declared static before; wouldn't compile.

--- BonsoirElliot.java
+++ BonsoirElliot.java
@@ -1,10 +1,10 @@
-/* 
- * Just prints "Bonsoir, Elliot" in the console 
+/*
+ * Just prints "Bonsoir, Elliot" in the console
  */
- 
+
 public class BonsoirElliot {
 
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Bonsoir, Elliot.");
 	}
 
